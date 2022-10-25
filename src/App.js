5e828bae03c832cf88ebc39e531af44819f5f314
Sakibhasan <hasansakib047@gmai.,com>
2022-10-25 23:30:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {Helmet} from "react-helmet";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
+import Main from "./Layouts/Main";
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:<Main></Main>,
+      children:[
+        {
+          path:"/",
+          
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Helmet>
+                <meta charSet="utf-8" />
+                <title>Learning Media</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta name="description" content="Learning Media" />
+            </Helmet>
+            <h1 className="3xl font-bold">ajdhksdha</h1>
+            <RouterProvider router={router }></RouterProvider>
     </div>
   );
 }
