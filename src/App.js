66e4556b732e2a03baf7dcc1d449439ecd4468e4
Sakibhasan <hasansakib047@gmai.,com>
2022-10-26 17:48:33 +0600

    fourth

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Courses from "./components/Courses/Courses";
 import Login from "./components/Login/Login";
-import Register from "./components/Register/Register"
+import Register from "./components/Register/Register";
 import Main from "./Layouts/Main";
 
 function App() {
-  const router=createBrowserRouter([
+  const router = createBrowserRouter([
     {
-      path:"/",
-      element:<Main></Main>,
-      children:[
+      path: "/",
+      element: <Main></Main>,
+      children: [
         {
-          path:"/",
-          element:<Courses></Courses>,
+          path: "/",
+          element: <Courses></Courses>,
         },
         {
-          path:"login",
-          element:<Login></Login>,
+          path: "login",
+          element: <Login></Login>,
         },
         {
-          path:"register",
-          element:<Register></Register>,
+          path: "register",
+          element: <Register></Register>,
+        },
+        {
+          path: "course/:id",
+          element:<Register></Register>
         }
-      ]
-    }
-  ])
+      ],
+    },
+  ]);
   return (
     <div className="App">
-       <Helmet>
-                <meta charSet="utf-8" />
-                <title>Learning Media</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-                <meta name="description" content="Learning Media" />
-            </Helmet>
-            
-            <RouterProvider router={router }></RouterProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Learning Media</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Learning Media" />
+      </Helmet>
+
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Courses = () => {
-    return (
-        <div>
-            
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+  return (
+    <div>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <img src={course.img} alt=" " />
+          <p>{course.name}</p>
+          <p>{course.views}</p>
+          <Link to={`/course/${course.id}`}>{course.name}</Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Courses;
